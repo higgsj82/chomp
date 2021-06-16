58db454a02d95e25778ad73eb9e5b4d541f275de
Jay Higgs <higgsj82@gmail.com>
2021-06-15 20:11:19 -0400

    created mongo database

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+const db = require('./config/keys').mongoURI;
 
+// set up mongo database
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('You did it!'))
+    .catch(err => console.log(err))
 
 // vvv server creation down below vvv
 app.get('/', (req, res) => res.send("App is now alive!"))
